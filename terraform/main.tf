e2365b39c08e0c0c5d258a9ef7f018d4d00d08c4
Roberth Troya <roberthdanielt@gmail.com>
2026-02-10 12:58:26 -0500

    fix main.tf remove outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -251,25 +251,3 @@ resource "aws_eip" "app_eip" {
     Name = "estudiantes-app-eip"
   }
 }
-
-# Outputs
-output "ec2_public_ip" {
-  description = "IP pública de la instancia EC2"
-  value       = aws_eip.app_eip.public_ip
-}
-
-output "rds_endpoint" {
-  description = "Endpoint de la base de datos RDS"
-  value       = aws_db_instance.estudiantes_db.endpoint
-  sensitive   = true
-}
-
-output "application_url" {
-  description = "URL de la aplicación"
-  value       = "http://${aws_eip.app_eip.public_ip}:8080"
-}
-
-output "api_base_url" {
-  description = "URL base de las APIs"
-  value       = "http://${aws_eip.app_eip.public_ip}:8080/api/v1"
-}
